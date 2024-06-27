9bb9ffb765c8710d5340d5a5dd21b60fdc1def7f
Dancel Mautsa <70059622+spicorn@users.noreply.github.com>
2024-06-27 21:50:53 +0200

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -63,7 +63,7 @@ const Contact = () => {
               type="email"
               name="user_email"
               pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
-              placeholder="exapmle@gmail.com"
+              placeholder="example@gmail.com"
               required
               className="border border-slate-600 p-3 rounded"
             />
