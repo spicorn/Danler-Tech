8a5d1cf58816cb972e0689474f957b06c7acffed
Dancel Mautsa <70059622+spicorn@users.noreply.github.com>
2024-07-21 00:54:49 +0200

    Update Hireme.jsx

--- src/components/Hireme.jsx
+++ src/components/Hireme.jsx
@@ -1,6 +1,6 @@
-import Lottie from "lottie-react";
 import { content } from "../Content";
-import animationData from "../assets/lottie/pointer.json";
+import { motion } from "framer-motion";
+import Logo from "../assets/images/Hero/danler.png";
 
 const Hireme = () => {
   const { Hireme } = content;
@@ -16,10 +16,21 @@ const Hireme = () => {
         </h4>
         <br />
         <div className="flex items-center md:flex-row flex-col-reverse ">
-          <Lottie
-            animationData={animationData}
-            data-aos="fade-right"
+          <motion.img
             className="max-w-sm md:block hidden"
+            src={Logo}
+            animate={{
+              scale: [1, 1.2, 1.3, 1, 1],
+              rotate: [0, 0, 180, 180, 0],
+              borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+            }}
+            transition={{
+              duration: 2,
+              ease: "easeInOut",
+              times: [0, 0.2, 0.5, 0.8, 1],
+              repeat: Infinity,
+              repeatDelay: 1,
+            }}
           />
 
           <div
@@ -28,6 +39,10 @@ const Hireme = () => {
            p-6 shadow-sm rounded-xl rounded-br-[8rem] sm:min-w-[22rem]"
           >
             <p className="leading-7">{Hireme.para}</p>
+            <br />
+            <button className="btn bg-dark_primary text-white">
+              {Hireme.btnText}
+            </button>
           </div>
         </div>
       </div>
